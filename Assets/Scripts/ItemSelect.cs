3b62a20912b21b359efde32c66961ed0050e1a51
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-09-26 02:16:19 +1000

    Adding selection script
    Created a script that catches players click on the object

---
+++ Assets/Scripts/ItemSelect.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSelect : MonoBehaviour
+{
+    //game over UI prefab reference
+    private void OnMouseDown()
+    {
+        //throw up game over UI prefab
+        //I could instead make it so the showing of win lose is in the game manager, then reference that?
+    }
+}
